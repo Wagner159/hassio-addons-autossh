106472529f73d98543e9948acb8ec1955956b233
Petr Kolář <kol.petrx@gmail.com>
2024-12-25 12:11:40 +0100

    added timeout parameter

--- hass_tunnel/run.sh
+++ hass_tunnel/run.sh
@@ -14,6 +14,7 @@ LOCAL_FORWARDING=$(jq --raw-output ".local_forwarding[]" $CONFIG_PATH)
 
 OTHER_SSH_OPTIONS=$(jq --raw-output ".other_ssh_options" $CONFIG_PATH)
 MONITOR_PORT=$(jq --raw-output ".monitor_port" $CONFIG_PATH)
+TIMEOUT_HOURS=$(jq --raw-output ".timeout_hours" $CONFIG_PATH)
 
 echo "[DEBUG] Start"
 
